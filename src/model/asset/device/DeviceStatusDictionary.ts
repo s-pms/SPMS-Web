12e8f5eda1959377f761514f99b8a2d1aad77435
Hamm <majhamm@qq.com>
2023-12-04 16:15:22 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/model/asset/device/DeviceStatusDictionary.ts
+++ src/model/asset/device/DeviceStatusDictionary.ts
@@ -1,11 +1,10 @@
 import { AirDictionaryArray } from '@/airpower/model/extend/AirDictionaryArray'
 import { DeviceStatus } from './DeviceStatus'
-import { AirColor } from '@/airpower/enum/AirColor'
 
 export const DeviceStatusDictionary = AirDictionaryArray.create([
-  { key: DeviceStatus.ALARM, label: '维护', color: AirColor.WARNING },
-  { key: DeviceStatus.RUNNING, label: '运行', color: AirColor.SUCCESS },
+  { key: DeviceStatus.ALARM, label: '报警', color: '#F5280C' },
+  { key: DeviceStatus.RUNNING, label: '运行', color: '#4ECF1F' },
   { key: DeviceStatus.FREE, label: '空闲', color: '#5a9cf8' },
-  { key: DeviceStatus.DOWN, label: '关机', color: AirColor.NORMAL },
-  { key: DeviceStatus.DEBUG, label: '维护', color: AirColor.WARNING },
+  { key: DeviceStatus.DOWN, label: '关机', color: '#606765' },
+  { key: DeviceStatus.DEBUG, label: '维护', color: '#1B24F7' },
 ])
