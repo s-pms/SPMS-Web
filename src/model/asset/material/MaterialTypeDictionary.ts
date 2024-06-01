5f3ad8524769dea249be82819c2d238d49f14df4
Hamm <majhamm@qq.com>
2024-06-01 17:33:58 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/model/asset/material/MaterialTypeDictionary.ts
+++
@@ -1,8 +0,0 @@
-import { AirDictionaryArray } from '@/airpower/model/extend/AirDictionaryArray'
-import { MaterialType } from './MaterialType'
-import { AirColor } from '@/airpower/enum/AirColor'
-
-export const MaterialTypeDictionary = AirDictionaryArray.create([
-  { key: MaterialType.PRODUCT, label: '自产品', color: AirColor.SUCCESS },
-  { key: MaterialType.PURCHASE, label: '外购品', color: AirColor.WARNING },
-])
