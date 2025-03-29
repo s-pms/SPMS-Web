aa504d164762fedeef550ae1afac7f51efe316b4
Hamm <majhamm@qq.com>
2025-03-30 06:26:26 +0800

    build:定 制 ESLint配置
    - 添加缩进风格和半角括号配置
    - 启用 TypeScript 支持
    - 禁用 .gitignore 文件检测
    - 忽略特定目录
    - 配置 Vue 文件块顺序

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,19 @@
 // eslint.config.js
 import config from '@antfu/eslint-config'
 
-export default config()
+export default config({
+  stylistic: {
+    indent: 2, // 缩进风格
+    semi: false,
+  },
+  typescript: true,
+  gitignore: false,
+  ignores: ['dist', '.idea', '.vscode'],
+  vue: {
+    overrides: {
+      'vue/block-order': ['error', {
+        order: ['script', 'template', 'style'],
+      }],
+    },
+  },
+})
