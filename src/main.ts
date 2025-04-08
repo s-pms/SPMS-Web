76c1b861590ef464f175cf2919dff47b72721015
Hamm <majhamm@qq.com>
2025-04-08 15:49:31 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,10 @@ import '@/assets/css/main.scss'
 AirConfig.appKey = 'spms'
 AirConfig.uploadUrl = '/api/file/upload'
 
+// 开启所有表格斑马纹
+// AirConfig.tableStripe = true
+
+// 开启所有表格边框线
+// AirConfig.tableBorder = true
+
 app.use(AirRouter.createRouter(routes)).mount('#app')
