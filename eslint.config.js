1e1625e11b2b92ccf9836fad16f45b116f50d3cc
Hamm <majhamm@qq.com>
2025-04-08 12:18:11 +0800

    fix(bugs): 修复了一些已知的BUG

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,13 @@ export default config({
       'vue/block-order': ['error', {
         order: ['script', 'template', 'style'],
       }],
+      'vue/max-attributes-per-line': [2, {
+        singleline: 3, // 一行不能超过 3 个属性，超过换行
+        multiline: {
+          max: 1, // 多行时，每行多少个属性
+          allowFirstLine: false,
+        },
+      }],
     },
   },
 })
