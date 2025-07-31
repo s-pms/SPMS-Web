10d51b17159809f78e9d04384bce4b848d2b8f43
Hamm <majhamm@qq.com>
2025-07-31 18:01:33 +0800

    fix(bugs): 修复了一些已知的BUG

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,26 @@
 import config from '@antfu/eslint-config'
 
 export default config({
-  stylistic: {
-    indent: 2,
-    semi: false,
-  },
-  typescript: true,
-  gitignore: true,
-  ignores: ['!airpower/**', '!src/**'],
-  vue: {
-    overrides: {
-      'vue/block-order': ['error', {
-        order: ['script', 'template', 'style'],
-      }],
+    stylistic: {
+        indent: 2,
+        semi: false,
     },
-  },
+    gitignore: false,
+    ignores: ['dist', '.idea', '.vscode'],
+    vue: {
+        overrides: {
+            'vue/block-order': ['error', {
+                order: ['script', 'template', 'style'],
+            }],
+            'vue/max-attributes-per-line': ['error', {
+                singleline: 1, // 单行最多 1 个属性
+                multiline: 1,
+            }],
+        },
+    },
+    typescript: {
+        overrides: {
+            'unicorn/error-message': 'off',
+        },
+    },
 })
